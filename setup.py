b00bfcdcb7d97aeefd2d1e0f9a949d91b991b034
Danilo Shiga <daniloshiga@gmail.com>
2012-08-22 08:17:24 -0300

    fixing application parameters on setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='Nerada',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='Personal Library Bookshelf Application',
+      author='Danilo Shiga',
+      author_email='daniloshiga@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['Flask>=0.7.2'],
      )
